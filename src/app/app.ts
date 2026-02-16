5995587f3bd3d648a92b0cb15ab957ecda50699e
SebastianWozniczka <sebastianmateuszwozniczka@gmail.com>
2026-02-16 16:59:33 +0100

    Expansion of the project.

--- src/app/app.ts
+++ src/app/app.ts
@@ -9,4 +9,52 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('L-Bud');
+
+  document: any;
+
+
+//str: String = "First ordered list item\n2. Another item\n * Unordered sub-list.\n"
+str: String = ""
+opacity: any = 0;
+
+
+
+  mouseEnter(div : string){
+
+      this.str = 'Szkolenia\nReferencje\n';
+      console.log("mouse enter : " + div);
+
+      var a = document.createElement('a');
+      var linkText = document.createTextNode("Szkolenia");
+      a.appendChild(linkText);
+      a.title = "my title text";
+      a.href = "http://example.com";
+
+      a.style.display != "none" ? "none" : "block";
+
+      this.opacity = 100;
+
+
+      //document.getElementsByClassName('firm')[0].innerHTML += "Szkolenia\nReferencje";
+
+      //document.getElementsByClassName('firm')[0].appendChild(a);
+     // document.body.appendChild(a);
+
+   }
+
+
+
+   mouseLeave(div : string){
+
+      var a = document.createElement('a');
+     // document.getElementsByClassName("firm")[0].removeAttribute("class");
+    // document.getElementsByClassName('firm')[0].removeChild(a)
+    //this.str.replace("\n", "<br>");
+    this.str = "";
+     console.log('mouse leave :' + div);
+
+      this.opacity = 0;
+
+   }
+
 }
---
+++ src/app/script1.js
@@ -0,0 +1,5 @@
+var paragraph = document.getElementByClass("firm");
+var text = document.createTextNode("This just got added");
+
+paragraph.appendChild(text);
+console.log("OK");
