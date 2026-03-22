fab314c5ed4d385b13c15b0bb09066490e22a9f4
SebastianWozniczka <sebastianmateuszwozniczka@gmail.com>
2026-03-22 20:01:17 +0100

    Beginning of view 2.

--- src/app/middle2/middle2.ts
+++ src/app/middle2/middle2.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-middle2',
@@ -7,5 +9,15 @@ import { Component } from '@angular/core';
   styleUrl: './middle2.css',
 })
 export class Middle2 {
+
+active = true;
+
+myAnim1() {
+
+     console.log("Animacja 1");
+
+
+}
+
 
 }
