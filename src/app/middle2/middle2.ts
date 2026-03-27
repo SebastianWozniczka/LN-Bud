6c3385b26af781d268f2c09b3a9558ff04998fbf
SebastianWozniczka <sebastianmateuszwozniczka@gmail.com>
2026-03-27 20:06:19 +0100

    Adding one animation

--- src/app/middle2/middle2.ts
+++ src/app/middle2/middle2.ts
@@ -12,12 +12,37 @@ export class Middle2 {
 
 active = true;
 
+
+opacity: any = 0;
+
+  showMore = true;
+
+
+
+  toggleShowMore(): void {
+    this.showMore = !this.showMore;
+  }
+
+
 myAnim1() {
+
 
      console.log("Animacja 1");
 
 
+      var a = document.createElement('a');
+      var linkText = document.createTextNode("Szkolenia");
+      a.appendChild(linkText);
+      a.title = "my title text";
+      a.href = "http://example.com";
+
+      a.style.display != "none" ? "none" : "block";
+
+      this.opacity = 100;
+
 }
+
+
 
 
 }
